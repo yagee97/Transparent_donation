7578cb30e0e0f8ee90aaf46c9ecce976330d7c8d
hjhng125 <33080926+hjhng125@users.noreply.github.com>
2018-11-03 19:01:42 +0900

    Login service
    Login service + MDM templete

---
+++ LoginTest/public/fireinit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCwaHlOrS9gomEiQ0hqgp2GtzvVqaI4eKs",
+    authDomain: "mdm-project-218502.firebaseapp.com",
+    databaseURL: "https://mdm-project-218502.firebaseio.com",
+    projectId: "mdm-project-218502",
+    storageBucket: "mdm-project-218502.appspot.com",
+    messagingSenderId: "741072852501"
+};
+firebase.initializeApp(config);
--- MyDigiMoney/app/src/main/gen/org/test/ahn/mydigimoney/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.test.ahn.mydigimoney;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
